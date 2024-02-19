f9d9b49434091306b35e14ee1613c4f8e087e0bb
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2024-02-18 16:54:45 -0800

    added currency check command

---
+++ commands/dabloons.ts
@@ -0,0 +1,25 @@
+import { CommandObject, CommandType } from 'wokcommands';
+import prisma from '../prisma/prisma';
+
+export default {
+  // Required for slash commands
+  description: 'Check your dabloon balance.',
+
+  // Create a legacy and slash command
+  type: CommandType.SLASH,
+
+  // Invoked when a user runs the ping command
+  callback: async ({ member }) => {
+    let user = await prisma.users.findUnique({
+      where: {
+        user_id: member?.id,
+      },
+    });
+
+    if (user === null) return { content: 'Ypu have no dabloons. Roll to get started!' };
+
+    return {
+      content: `${member} has ${user.currency} dabloons!`,
+    };
+  },
+} as CommandObject;
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Client, IntentsBitField, Partials } from 'discord.js';
+import { Client, Events, IntentsBitField, Partials } from 'discord.js';
 import WOK from 'wokcommands';
 import path from 'path';
 require('dotenv/config');
@@ -7,13 +7,16 @@ const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.DirectMessages,
     IntentsBitField.Flags.MessageContent,
   ],
   partials: [Partials.Channel],
 });
 
-client.on('ready', () => {
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
   client.user?.setPresence({
     status: 'online',
     activities: [
@@ -29,15 +32,13 @@ client.on('ready', () => {
     commandsDir: path.join(__dirname, 'commands'),
     botOwners: ['95377567707566080'],
     testServers: ['289669662612914176'],
-    //mongoUri: process.env.DATABASE_URL,
+    mongoUri: process.env.DATABASE_URL,
     cooldownConfig: {
       errorMessage: 'Please wait {TIME} before doing that again.',
       botOwnersBypass: true,
       dbRequired: 300,
     },
   });
-
-  console.log('The bot is ready');
 });
 
 client.login(process.env.TOKEN);
